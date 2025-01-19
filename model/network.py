b30d0a189bf158ee7ffd3138873b48ff6da3b38b
wenbo <1443221471@qq.com>
2025-01-19 10:45:53 +0800

    优化筛选步骤的逻辑，去掉DCT变换类，封装成方法。

--- model/network.py
+++ model/network.py
@@ -1,25 +1,24 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch import Tensor
+from torchjpeg.dct import block_dct
 from tqdm import tqdm
 from dataset import CDDataset
-from model.help_function import size_restore
-from model.module_coarse_detection import CoarseDetection
-from model.module_dpffe import DualPerspectiveFrequencyFeatureExtractor
-# from model.cdd_Dpffe import FreTransformer
+from model.help_function import size_restore, cut, apply_dct
+from model.module_dpffe import DualPerFreFeatureExtractor
 from model.to_dct import ToDCT
-from model.backbone import ResNet
-from model.module_sfif import SpaFreInteractionFusionModule
-# from model.cdd_Sfif import SpaFreInteractionFusionModule
-from configs.LEVIR_CD import pretrain_model_path_ResNet18
+from module_coarse_detection import CoarseDetection
+from model.module_sfif import SpaFreInteractionFusion
 from model.module_fph import FreFeaturePred
-# from model.cdd_fre_pred import FreFeaturePred
+
+
 
 
 class Network(nn.Module):
     def __init__(self,
                  backbone='resnet18', stages_num=4,
-                 threshold=0.5, phase='val', dct_size=4, patch_size=64,
+                 threshold=0.5, phase='val', dct_size=4, block_size=64,
                  encoder_depth=1, encoder_heads=8, encoder_dim=8, decoder_depth=1,
                  decoder_heads=4, decoder_dim=8,
                  dropout=0.5):
@@ -34,19 +33,17 @@ class Network(nn.Module):
                 self.channel = 1024
             else:
                 self.channel = 3904
-        self.dct_size = dct_size
-        self.patch_size = patch_size
-        self.patch_num_large = (256 // patch_size) ** 2
+        self.dct_patch_size = dct_size
+        self.block_size = block_size
+        self.patch_num_large = (256 // block_size) ** 2
         self.phase = phase
-        self.backbone = ResNet(backbone=backbone, resnet_stages_num=stages_num)
-        self.coarse = Coarse(stages_num=stages_num, threshold=threshold, patch_size=patch_size, channel=self.channel,
-                             phase=phase)
-        self.to_dct = ToDCT(dct_size=dct_size, patch_size=patch_size)
-        self.freTransformer = DualPerspectiveFrequencyFeatureExtractor(dct_size=dct_size, patch_size=patch_size,
-                                                                       encoder_depth=encoder_depth, encoder_heads=encoder_heads, encoder_dim=encoder_dim,
-                                                                       decoder_depth=decoder_depth, decoder_heads=decoder_heads, decoder_dim=decoder_dim, dropout=dropout)
-        self.frepred = FreFeaturePred(dct_size=dct_size, patch_size=patch_size)
-        self.sfifm = SpaFreInteractionFusionModule(patch_size=patch_size)
+        self.coarse_detection = CoarseDetection(backbone)
+        self.to_dct = ToDCT(dct_size=dct_size, patch_size=block_size)
+        self.dpffe = DualPerFreFeatureExtractor(dct_size=dct_size, patch_size=block_size,
+                                                         encoder_depth=encoder_depth, encoder_heads=encoder_heads, encoder_dim=encoder_dim,
+                                                         decoder_depth=decoder_depth, decoder_heads=decoder_heads, decoder_dim=decoder_dim, dropout=dropout)
+        self.fph = FreFeaturePred(dct_size=dct_size, patch_size=block_size)
+        self.sfif = SpaFreInteractionFusion(patch_size=block_size)
         self.pred_head2 = nn.Sequential(
             # nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=1, padding=1,),
@@ -76,21 +73,18 @@ class Network(nn.Module):
         #     # nn.ReLU(inplace=True),
         #     # nn.Conv2d(in_channels=2, out_channels=1, kernel_size=3, stride=1, padding=1, ),
         # )
-        pretrain_dict = torch.load(pretrain_model_path_ResNet18)
-        self.backbone.resnet.load_state_dict(pretrain_dict)
 
-    def forward(self, x1, x2):
-        b = int(x1.shape[0])
-
+    def forward(self, x0, x1):
+        b = int(x0.shape[0])
         """
-        <----------- backbone and pred head 1 ---------->
+        <----------- stage one --> coarse detection ---------->
         """
-        feature = [self.backbone(x1), self.backbone(x2)]
-        coarse_prob, coarse_score, fine_idx2, feature, finetune_mask = self.coarse(feature)
+        f0_spa, f1_spa, coarse_score = self.coarse_detection(x0, x1)
+        '''
+        <---------- block cut and DCT for every block ----------- >
+        '''
+        fine_idx2, mask = self.get_filter_idx(coarse_score)
         fine_idx = torch.arange(0, b * self.patch_num_large).cuda()
-        '''
-        <---------- blocky 64 and DCT for every 64x patch ----------- >
-        '''
         if fine_idx2.numel() == 0:
             if self.phase == 'val' or 'test':
                 coarse2_score_ = 0
@@ -102,23 +96,24 @@ class Network(nn.Module):
         else:
             if self.phase == 'val' or 'test':
                 fine_idx = fine_idx2
-        coarse_prob_sel = F.unfold(coarse_score, kernel_size=(self.patch_size, self.patch_size),
-                                          padding=0, stride=(self.patch_size, self.patch_size))
-        coarse_prob_sel = coarse_prob_sel.transpose(1, 2).reshape(-1, self.patch_size, self.patch_size)
-        coarse_prob_sel = torch.index_select(coarse_prob_sel, dim=0, index=fine_idx2.squeeze())
 
-        x1, x2 = self.to_dct(x1, x2, fine_idx)
         '''
-        <----------two kind of transformer block-------->
+        <---------- block cut and DCT ----------- >
         '''
-        x_fre = self.freTransformer(x1, x2, fine_idx)
+        x = torch.cat([x0, x1], dim=1)    # N, C+C, H, H
+        blocks = cut(x, self.block_size)          # N, L, C+C, block_size, block_size
+        b0, b1 = self.select_and_dct(blocks, fine_idx).chunk(2, dim=2)
+        '''
+        <---------- stage two --> fine detection-------->
+        '''
+        f1, f2 = self.dpffe()
         '''
         <---------- feature from fre transformer pred---------->
         '''
         coarse3_score, x_fre = self.frepred(x_fre)       # bs16, 1, 64, 64
         if self.phase == 'train':
-            coarse3_score = coarse3_score.view(b, -1, self.patch_size**2)
-            coarse3_score = size_restore(coarse3_score, input_size=self.patch_size, output_size=256)
+            coarse3_score = coarse3_score.view(b, -1, self.block_size ** 2)
+            coarse3_score = size_restore(coarse3_score, input_size=self.block_size, output_size=256)
         else:
             coarse3_score = 0
         # coarse3_prob = torch.sigmoid(coarse3_score)
@@ -129,10 +124,10 @@ class Network(nn.Module):
         t0, t1 = self.sfifm(feature, x_fre, coarse_prob_sel, fine_idx2)
         coarse2_score_ = torch.abs(t0 - t1)
         coarse2_score_ = self.pred_head2(coarse2_score_)
-        coarse2_score = torch.zeros(b*self.patch_num_large, 1, self.patch_size, self.patch_size).to(coarse2_score_.dtype).cuda()
+        coarse2_score = torch.zeros(b * self.patch_num_large, 1, self.block_size, self.block_size).to(coarse2_score_.dtype).cuda()
         coarse2_score[fine_idx2, :, :, :] = coarse2_score_
-        coarse2_score = coarse2_score.view(b, -1, self.patch_size**2)
-        coarse2_score = size_restore(coarse2_score, input_size=self.patch_size, output_size=256)
+        coarse2_score = coarse2_score.view(b, -1, self.block_size ** 2)
+        coarse2_score = size_restore(coarse2_score, input_size=self.block_size, output_size=256)
         '''
         <-------- three branch pred cm fusion------->
         '''
@@ -141,6 +136,58 @@ class Network(nn.Module):
         # return coarse_prob, coarse2_score, coarse3_score, fine, fine_idx2
         return coarse_score, coarse2_score_, coarse3_score, fine, fine_idx2
         # return coarse3_score
+
+    def block_filter(self, coarse_score, fine_idx2):
+        coarse_prob_sel = F.unfold(coarse_score, kernel_size=(self.block_size, self.block_size),
+                                   padding=0, stride=(self.block_size, self.block_size))
+        coarse_prob_sel = coarse_prob_sel.transpose(1, 2).reshape(-1, self.block_size, self.block_size)
+        coarse_prob_sel = torch.index_select(coarse_prob_sel, dim=0, index=fine_idx2.squeeze())
+        return coarse_prob_sel
+
+
+    def get_filter_idx(self, coarse_score):
+        coarse_prob = torch.sigmoid(coarse_score)
+        coarse_mask = torch.tensor(coarse_prob > self.threshold)
+        fine_index = None
+        if self.phase == 'train':  # 训练时用所有patch
+            fine_index = torch.ones(coarse_mask.shape[0] * (256 // self.block_size) ** 2).cuda()
+        elif self.phase == 'val' or 'test':  # 推理时进行筛选
+            fine_index = F.unfold(coarse_mask, kernel_size=(self.block_size, self.block_size),
+                                  padding=0, stride=(self.block_size, self.block_size))  # bs * 4096 * 16
+            fine_index = (fine_index.transpose(1, 2).sum(dim=2)).flatten()
+            fine_index = torch.gt(fine_index, 0) & torch.lt(fine_index, self.block_size ** 2)
+
+        mask = torch.ones(coarse_mask.shape[0] * (256 // self.block_size) ** 2, 1, self.block_size,
+                          self.block_size).cuda()
+        mask = (mask * fine_index.view(coarse_mask.shape[0] * (256 // self.block_size) ** 2, 1, 1, 1))
+        fine_index = torch.nonzero(fine_index)
+
+        mask = mask.view(coarse_mask.shape[0], (256 // self.block_size) ** 2, -1).transpose(1, 2)
+        mask = F.fold(mask, kernel_size=(self.block_size, self.block_size), output_size=(256, 256),
+                      dilation=1, padding=0,
+                      stride=(self.block_size, self.block_size))
+
+        # return coarse_prob, coarse_score, fine_index, finetune_mask, spafeature, raw_feature
+        # coarse_score_selection = coarse_score_selection.view(-1, self.patch_num, self.patch_size, self.patch_size)
+        return fine_index.squeeze(), mask
+
+    def select_and_dct(self, x, fine_idx):
+        """
+        根据idx筛选block, 并且做dct
+        :param x: 切分后的block, shape: N * block_num(B) * C * block_size * block_size
+        :param fine_idx: 保留的block的索引
+        :return: x的dct并且做zigzag展开成一维向量, shape: (NB) * patch_num * C * dct_size^2
+        """
+        b, l, c, block_size, block_size = x.shape
+        x = torch.index_select(x, dim=0, index=fine_idx)
+        x = x.view(-1, c, block_size, block_size)
+        x = apply_dct(x, self.dct_patch_size)
+        return x
+
+
+
+
+
 
 if __name__ == '__main__':
     from torchsummary import summary
