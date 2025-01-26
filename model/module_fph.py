53c284e9e279908a67b01d08830660f26ab6ab76
Beat <1443221471@qq.com>
2025-01-26 22:30:33 +0800

    非训练时频域不进行预测任务。

--- model/module_fph.py
+++ model/module_fph.py
@@ -2,27 +2,26 @@ import torch
 import torch.nn.functional as F
 import torch.nn as nn
 from torchjpeg import dct
-from model.help_function import inverse_zigzag, size_restore
+from model.help_function import inverse_zigzag
 
 
 class FreFeaturePred(nn.Module):
-    def __init__(self,
-                 dct_size=4,
-                 block_size=64):
+    def __init__(self, phase, dct_size=4, block_size=32):
         super(FreFeaturePred, self).__init__()
+        self.phase = phase
         self.dct_size = dct_size
         self.block_size = block_size
         self.h = block_size // dct_size
         self.bands = dct_size ** 2
         self.patch_num = (block_size // dct_size) ** 2
-        self.conv1 = nn.Sequential(
+        self.conv = nn.Sequential(
             nn.Conv2d(6, 32, 1, 1, 0),
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
-            nn.Conv2d(32, 64, 3, 1, 1),
+            nn.Conv2d(32,  64, 3, 1, 1),
         )
 
-        self.pred_f2s = nn.Sequential(
+        self.pred_head_f2s = nn.Sequential(
             nn.Conv2d(64, 32, 3, 1, 1),
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
@@ -31,7 +30,6 @@ class FreFeaturePred(nn.Module):
 
         self.fre_embedding = nn.Sequential(
             nn.Linear(6 * self.bands, 64),
-            # nn.Linear(3 * self.dct_size ** 2, 64),
             nn.BatchNorm1d(self.patch_num),
             nn.ReLU(),
             nn.Linear(64, 128),
@@ -39,7 +37,7 @@ class FreFeaturePred(nn.Module):
             nn.ReLU(),
             nn.Linear(128, 256),
         )
-        self.pred_head = nn.Sequential(
+        self.pred_head_fre = nn.Sequential(
             nn.Linear(256, 128),
             nn.BatchNorm1d(self.patch_num),
             nn.ReLU(),
@@ -59,35 +57,36 @@ class FreFeaturePred(nn.Module):
             nn.ConvTranspose2d(128, 64, 3, 2, 1, output_padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
             nn.ConvTranspose2d(64, 32, 3, 2, 1, output_padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(32, 32, 3, 1, 1),
             nn.BatchNorm2d(32),
-            # nn.Conv2d(64, 32, 3, 1, 1),
-            # nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(32, 1, 3, 1, 1, padding_mode='replicate'),
         )
 
     def forward(self, x0, x1):
-        # <-------频域转换回空间域-------->
         f0 = self._idct_and_feature_fre_size_restore(x0)  # bs16, 6, 64, 64
-        f1 = self._idct_and_feature_fre_size_restore(x1 )
+        f1 = self._idct_and_feature_fre_size_restore(x1)
         x_fre = torch.cat([f0.clone(), f1.clone()], dim=1)
-        # <------空间域预测------->
-        f0_f2s = self.conv1(f0)
-        f1_f2s = self.conv1(f1)
-        pred_score_f2s = torch.abs(f0_f2s - f1_f2s)
-        pred_score_f2s = self.pred_f2s(pred_score_f2s)
+        if self.phase != 'train':
+            return None, x_fre
 
-        # <------频域预测------->
+        f0_f2s = self.conv(f0)
+        f1_f2s = self.conv(f1)
+        pred_score_f2s = torch.abs(f0_f2s - f1_f2s)
+        pred_score_f2s = self.pred_head_f2s(pred_score_f2s)
+
         bs_patch = x_fre.shape[0]
         t0_fre = f0.view(bs_patch, -1, 6 * self.dct_size ** 2)
         t1_fre = f1.view(bs_patch, -1, 6 * self.dct_size ** 2)
         t0_fre = self.fre_embedding(t0_fre)
         t1_fre = self.fre_embedding(t1_fre)
-        pred_score_fre = self.pred_head(torch.abs(t0_fre - t1_fre))
+        pred_score_fre = self.pred_head_fre(torch.abs(t0_fre - t1_fre))
         pred_score_fre = pred_score_fre.view(bs_patch, 1, self.h, self.h)
 
         pred_score_fre = self.deconv(pred_score_fre)
@@ -95,7 +94,6 @@ class FreFeaturePred(nn.Module):
         pred_score = pred_score_f2s + pred_score_fre
 
         return pred_score, x_fre
-        # return x_fre
 
     def _idct_and_feature_fre_size_restore(self, x):
         bs_block, n, c, _ = x.shape
@@ -106,11 +104,10 @@ class FreFeaturePred(nn.Module):
         x = x.permute(0, 2, 3, 1).reshape(-1, self.badns, n)
         x = F.fold(x, kernel_size=(self.dct_size, self.dct_size), output_size=(self.block_size, self.block_size),
                    dilation=1, padding=0, stride=(self.dct_size, self.dct_size))
-        # x = x.view(b_patch, c, self.patch_size, self.patch_size)
         x = x.view(bs_block, c, -1)
         min_value = torch.min(x, dim=2, keepdim=True)[0]
         max_value = torch.max(x, dim=2, keepdim=True)[0]
-        # 将张量归一化到[-1, 1]的范围
+        # norm x to [-1, 1]
         x = 2 * (x - min_value) / (max_value - min_value) - 1
         return x.view(bs_block, c, self.block_size, self.block_size)
 
