82cabdacd65096a238eb0209dd68976a7c61a58c
wenbo <1443221471@qq.com>
2025-01-28 17:24:27 +0800

    以项目路径导入

--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,7 @@
-from module_coarse_detection import CoarseDetection
-from module_sfif import SpaFreInteractionFusion
-from module_fph import FreFeaturePred
-from module_dpffe import DualPerFreFeatureExtractor
-from module_coarse_detection import CoarseDetection
-from module_fie import FreInfoExchange
-from help_function import size_restore, cut, apply_dct
+from model.module_coarse_detection import CoarseDetection
+from model.module_sfif import SpaFreInteractionFusion
+from model.module_fph import FreFeaturePred
+from model.module_dpffe import DualPerFreFeatureExtractor
+from model.module_fie import FreInfoExchange
+from model.help_function import size_restore, cut, apply_dct
+from model.network import CDC2F
