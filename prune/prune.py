76d2c1a3280d14738be61a032369f8b8e7f8358c
Beat <1443221471@qq.com>
2025-02-05 21:23:17 +0800

    改造成命令行传参

--- prune/prune.py
+++ prune/prune.py
@@ -1,16 +1,15 @@
-import os
+import argparse
 import re
 
-import torch
-from tqdm import tqdm
-import configs as cfg
 import torch_pruning as tp
-from hook import *
-from similarity import similarity_for_single_layer
+from tqdm import tqdm
+
+import configs as cfg
 from dataset import get_data_loader
-from torchsummary import summary
-from thop import profile
+from hook import *
 from model import CDC2F
+from similarity import similarity_for_single_layer
+
 resnet_blocks = {
     'resnet18': [2, 2, 2, 2],
     'resnet34': [3, 4, 6, 3],
@@ -20,8 +19,8 @@ resnet_blocks = {
 
 def prune(model, layer_groups):
     """
-    :param layer_groups: 将所有卷积层分组，每组的格式为: layers-channle idx
-    :param prune_strategy: 按topk剪枝还是按照平均值剪枝
+    param: model: 待剪枝的模型
+    param: layer_groups: 将所有卷积层分组，每组的格式为: layers-channel idx
     """
     DG = tp.DependencyGraph()
     DG.build_dependency(model, example_inputs=(torch.randn(1, 3, 256, 256).cuda(), torch.randn(1, 3, 256, 256).cuda()))
@@ -33,40 +32,57 @@ def prune(model, layer_groups):
     print(model)
     return model
 
-def params_flops(model):
-    summary(model, [(3, 256, 256), (3, 256, 256)])
-    flops, params = profile(model.cuda(), (torch.randn(1, 3, 256, 256).cuda(), torch.randn(1, 3, 256, 256).cuda()))
-    print(f"模型的FLOPs: {flops / 1e9} G FLOPs")  # 以十亿FLOPs为单位显示
-    print(f"模型的参数数量: {params / 1e6} M")
-    total = sum([param.nelement() for param in model.parameters()])
-    print('Number of parameter: % .4fM' % (total / 1e6))
-
+def get_prune_idx(dists, prune_strategy, prune_factor=None):
+    channel_idx = []
+    for dist in dists:
+        dist = torch.tensor(dist)
+        idx = None
+        if prune_strategy == 'topk':
+            _, idx = torch.topk(dist, round(len(dist) * prune_factor), largest=False)
+            idx = idx.tolist()
+        elif prune_strategy == 'mean':
+            mean = torch.mean(dist)
+            idx = [i for i, x in enumerate(dist) if x > mean]
+        channel_idx.append(idx)
+    return channel_idx
 
 if __name__ == '__main__':
-    prune_strategy = 'topk'
+    # ----- args parser -----
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--backbone', type=str, default='resnet50',
+                        help='backbone, include resnet18/34/50/101, default: resnet50')
+    parser.add_argument('--prune_strategy', type=str, default='topk',
+                        help='prune strategy, topk / mean')
+    parser.add_argument('--prune_factor', type=float, default=0.5)
+    parser.add_argument('--pth_path', type=str)
+    args = parser.parse_args()
 
-    prune_factor1 = 0.9
-    prune_factor2 = 0.9
-
-    model = CDC2F(cfg.backbone, stages_num=5, phase='train', backbone_pretrained=True).eval().cuda()
+    # ----- model -----
+    model = CDC2F(args.backbone, stages_num=5, phase='val', backbone_pretrained=True).eval().cuda()
     print(model)
-    # params_flops(model)
-    state_dict = torch.load(os.path.join(cfg.training_best_ckpt, 'LEVIR_CD_origin_resnet101_best.pth'))
+    state_dict = torch.load(args.ph_path)
     model.load_state_dict(state_dict['model_state'])
-    val_loader = get_data_loader(cfg.data_path, 'val', 16, cfg.val_txt_path)
     register_hook(model)
 
-    # pattern1 = r'^resnet\.layer\d+\.\d+\.conv1$'
-    # pattern2 = r'^resnet\.layer\d+\.\d+\.conv2$'
+    # ----- dataset -----
+    val_loader = get_data_loader(cfg.data_path, 'val', 16, cfg.val_txt_path, drop_last=False)
 
-    pattern1 = r'^resnet\.layer\d+\.\d+\.conv[12]$'
-    pattern2 = r'^resnet\.layer\d+\.\d+\.conv3$'
+    # ----- prepare layers -----
+    if args.backbone in ['resnet18', 'resnet34']:
+        single_layer = r'^resnet\.layer\d+\.\d+\.conv1$'
+        shortcut_layer = r'^resnet\.layer\d+\.\d+\.conv2$'
+        shortcut_layer_last = r'^resnet\.layer\d+\.1\.conv2$'
+    else:
+        single_layer = r'^resnet\.layer\d+\.\d+\.conv[12]$'
+        shortcut_layer = r'^resnet\.layer\d+\.\d+\.conv3$'
+        shortcut_layer_last = r'^resnet\.layer\d+\.1\.conv3$'
+
     group_single = {}
     group_shortcut = {}
     for layer, name in layer_dict.items():
-        if re.match(pattern1, name):
+        if re.match(single_layer, name):
             group_single[name] = layer
-        elif re.match(pattern2, name):
+        elif re.match(shortcut_layer, name):
             group_shortcut[name] = layer
 
     print('----------------single layer----------------')
@@ -77,78 +93,62 @@ if __name__ == '__main__':
     for name, module in group_shortcut.items():
         print(f"Layer: {name}, Module: {module}")
 
-    conv1_dist1 = {layer: [] for layer in group_single.keys()}
-    conv2_dist2 = [[], [], [], []]
+    # ----- inference and calculate similarity -----
+    dist_single = {layer: [] for layer in group_single.keys()}
+    dist_shortcut = [[], [], [], []]
     model.zero_grad()
-    for idx, batch in enumerate(tqdm(val_loader)):
-        outputs_clear()
-        img1, img2, _ = batch
-        img1 = img1.cuda()
-        img2 = img2.cuda()
-        with torch.no_grad():
+    with torch.no_grad():
+        for idx, batch in enumerate(tqdm(val_loader)):
+            outputs_clear()
+            img1, img2, _ = batch
+            img1 = img1.cuda()
+            img2 = img2.cuda()
+
             model(img1, img2)
 
-        for name, layer in group_single.items():
-            dist = similarity_for_single_layer(layer_outputs[name][0], layer_outputs[name][1], 'euc')
-            if len(conv1_dist1[name]) == 0:
-                conv1_dist1[name] = dist
-            else:
-                conv1_dist1[name] = [a + b for a, b in zip(conv1_dist1[name], dist)]
+            # ----- dist of single layer -----
+            for name, layer in group_single.items():
+                dist = similarity_for_single_layer(layer_outputs[name][0], layer_outputs[name][1], 'cos')
+                if len(dist_single[name]) == 0:
+                    dist_single[name] = dist
+                else:
+                    dist_single[name] = [a + b for a, b in zip(dist_single[name], dist)]
 
-        cur_layer = 0
-        layer_list = list(group_shortcut.keys())
-        for i in range(len(resnet_blocks[cfg.backbone])):  # [2, 2, 2, 2]
-            end_layer = cur_layer + resnet_blocks[cfg.backbone][i]
-            dists = []
-            for cur_pos in range(cur_layer, end_layer):
-                layer_name = layer_list[cur_pos]
-                dist = similarity_for_single_layer(layer_outputs[layer_name][0], layer_outputs[layer_name][1], 'euc')
-                dists.append(dist)
-            dist_sum = [sum(x) for x in zip(*dists)]
-            cur_layer = end_layer
+            # ----- dist of layer with shortcut for each stage-----
+            cur_layer = 0
+            layer_list = list(group_shortcut.keys())
+            for stage in range(4):  # eg. [2, 2, 2, 2]
+                end_layer = cur_layer + resnet_blocks[args.backbone][stage]
+                dists = []
+                for cur_pos in range(cur_layer, end_layer):
+                    layer_name = layer_list[cur_pos]
+                    dist = similarity_for_single_layer(layer_outputs[layer_name][0], layer_outputs[layer_name][1], 'euc')
+                    dists.append(dist)
+                dist_sum = [sum(x) for x in zip(*dists)]
+                cur_layer = end_layer
+                if len(dist_shortcut[stage]) == 0:
+                    dist_shortcut[stage] = dist_sum
+                else:
+                    dist_shortcut[stage] = [a + b for a, b in zip(dist_shortcut[stage], dist_sum)]
 
-            if len(conv2_dist2[i]) == 0:
-                conv2_dist2[i] = dist_sum
-            else:
-                conv2_dist2[i] = [a + b for a, b in zip(conv2_dist2[i], dist_sum)]
-
-    channel_idx1 = []
-    for channel_dist in conv1_dist1.values():
-        channel_dist = torch.tensor(channel_dist)
-        if prune_strategy == 'topk':
-            _, idx = torch.topk(channel_dist, round(len(channel_dist) * prune_factor1), largest=False)
-            idx = idx.tolist()
-        elif prune_strategy == 'std':
-            mean = torch.mean(channel_dist)
-            std = torch.std(channel_dist)
-            idx = [i for i, x in enumerate(channel_dist) if x > mean]
-        channel_idx1.append(idx)
+    # ----- get prune idx for each layer -----
+    channel_idx1 = get_prune_idx(dist_single.values(), args.prune_strategy, args.prune_factor)
     layer_channel1 = dict(zip([module for _, module in group_single.items()], channel_idx1))
 
-    channel_idx2 = []
-    for channel_dist in conv2_dist2:
-        channel_dist = torch.tensor(channel_dist)
-        if prune_strategy == 'topk':
-            _, idx = torch.topk(channel_dist, round(len(channel_dist) * prune_factor2), largest=False)
-            idx = idx.tolist()
-        elif prune_strategy =='mean':
-            mean = torch.mean(channel_dist)
-            std = torch.std(channel_dist)
-            idx = [i for i, x in enumerate(channel_dist) if x > mean]
-        channel_idx2.append(idx)
-    pattern3 = r'^resnet\.layer\d+\.1\.conv3$'
-    # pattern3 = r'^resnet\.layer\d+\.1\.conv2$'
-    conv2_group_ = []
+    channel_idx2 = get_prune_idx(dist_shortcut, args.prune_strategy, args.prune_factor)
+    shortcut_prune_group = []
     for name, module in group_shortcut.items():
-        if re.match(pattern3, name):
-            conv2_group_.append(module)
-    layer_channel2 = dict(zip(conv2_group_, channel_idx2))
+        if re.match(shortcut_layer_last, name):
+            shortcut_prune_group.append(module)
+    layer_channel2 = dict(zip(shortcut_layer_last, channel_idx2))
 
-    remove_hook(handles)
+    remove_hook(handles) # remove hook, or the next inference will have problem
+
+    # ----- prune -----
     pruned_model = prune(model, [layer_channel1, layer_channel2])
-    if prune_strategy == 'topk':
-        pruned_model_name =  f'pruned_{cfg.backbone}_top{int(prune_factor1*100)}.pth'
+    if args.prune_strategy == 'topk':
+        pruned_model_name =  f'pruned_{args.backbone}_top{int(args.prune_factor*100)}.pth'
     else :
-        pruned_model_name = f'pruned_{cfg.backbone}_mean_std.pth'
+        pruned_model_name = f'pruned_{args.backbone}_mean_std.pth'
     torch.save(pruned_model, pruned_model_name)
     # params_flops(pruned_model)
